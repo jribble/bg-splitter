66ec27a7baafe9a69842f2d1ca463be070f27c43
Jarrod Ribble <jarrod.ribble@gmail.com>
2018-09-12 11:21:06 -0600

    Add the ability to mark a pane as hidden, which will full size the other pane. Only one pane is allowed to be hidden at a time.

--- js/splitter.js
+++ js/splitter.js
@@ -21,9 +21,47 @@ angular.module('bgDirectives', [])
         return $scope.panes.length;
       };
 
+      this.hidePane = function (index) {
+        var visiblePane = index === 0 ? $scope.panes[1] : $scope.panes[0];
+        if (visiblePane.hidePane === true) {
+          visiblePane.hidePane = false;
+        }
+
+        var vertical = $scope.orientation == 'vertical';
+        var bounds = $scope.element[0].getBoundingClientRect();
+        $scope.handler.hide();
+        if (vertical) {
+          var height = bounds.bottom - bounds.top;
+          visiblePane.elem.css('top', '0px');
+          if (index === 0) {
+            visiblePane.elem.css('height', height + 'px');
+          }
+
+        } else {
+          var width = bounds.right - bounds.left;
+          visiblePane.elem.css('left', '0px');
+          if (index === 0) {
+            visiblePane.elem.css('width', width + 'px');
+          }
+        }
+        if ($scope.onChange) {
+          $timeout($scope.onChange);
+        }
+      };
+
+      this.showPane = function () {
+        $scope.handler.show();
+        $scope.loadSavedPosition();
+        if ($scope.onChange) {
+          $timeout($scope.onChange);
+        }
+      };
+
     },
     link: function (scope, element, attrs) {
-      var handler = angular.element('<div class="split-handler"></div>');
+      scope.element = element;
+
+      var handler = scope.handler = angular.element('<div class="split-handler"></div>');
       var pane1 = scope.panes[0];
       var pane2 = scope.panes[1];
       var vertical = scope.orientation == 'vertical';
@@ -54,12 +92,8 @@ angular.module('bgDirectives', [])
 
         if (vertical) {
           var height = bounds.bottom - bounds.top;
-          if (pos < pane1Min) {
-            pos = pane1Min;
-          }
-          if (height - pos < pane2Min) {
-            pos = height - pane2Min;
-          }
+          if (pos < pane1Min) pos = pane1Min;
+          if (height - pos < pane2Min) pos = height - pane2Min;
 
           handler.css('top', pos + 'px');
           pane1.elem.css('height', pos + 'px');
@@ -67,12 +101,8 @@ angular.module('bgDirectives', [])
 
         } else {
           var width = bounds.right - bounds.left;
-          if (pos < pane1Min) {
-            pos = pane1Min;
-          }
-          if (width - pos < pane2Min) {
-            pos = width - pane2Min;
-          }
+          if (pos < pane1Min) pos = pane1Min;
+          if (width - pos < pane2Min) pos = width - pane2Min;
 
           handler.css('left', pos + 'px');
           pane1.elem.css('width', pos + 'px');
@@ -85,9 +115,7 @@ angular.module('bgDirectives', [])
       };
 
       element.bind('mousemove', function (ev) {
-        if (!drag) {
-          return;
-        }
+        if (!drag) return;
 
         var bounds = element[0].getBoundingClientRect();
         var pos = 0;
@@ -116,8 +144,10 @@ angular.module('bgDirectives', [])
         drag = false;
       });
 
-      function loadSavedPosition() {
-        if ($(element).is(':visible')) {
+      var loadSavedPosition = scope.loadSavedPosition = function () {
+        if ($(element).is(':visible') && scope.panes.every(function (p) {
+          return p.hidePane !== true;
+        })) {
           lastPos = getSavedPosition();
           if (lastPos === null) {
             var bounds = element[0].getBoundingClientRect();
@@ -126,7 +156,7 @@ angular.module('bgDirectives', [])
 
           setPosition(lastPos);
         }
-      }
+      };
 
       $timeout(loadSavedPosition);
 
@@ -134,26 +164,40 @@ angular.module('bgDirectives', [])
             return $(element).is(':visible');
           },
           function () {
-            if ($(element).is(':visible')) {
-              $timeout(loadSavedPosition);
-            }
+            if ($(element).is(':visible')) $timeout(loadSavedPosition);
           });
     }
   };
 })
-.directive('bgPane', function () {
+.directive('bgPane', function ($timeout) {
   return {
     restrict: 'E',
     require: '^bgSplitter',
     replace: true,
     transclude: true,
     scope: {
-      minSize: '='
+      minSize: '=',
+      hidePane: '=?'
     },
-    template: '<div class="split-pane{{index}}" ng-transclude></div>',
+    template: '<div class="split-pane{{index}}" ng-hide="hidePane" ng-transclude></div>',
     link: function (scope, element, attrs, bgSplitterCtrl) {
+      if (scope.hidePane === undefined) {
+        scope.hidePane = false;
+      }
       scope.elem = element;
       scope.index = bgSplitterCtrl.addPane(scope);
+
+      $timeout(function () {
+        scope.$watch('hidePane', function (newVal, oldVal) {
+          if (newVal !== undefined && newVal !== oldVal) {
+            if (newVal) {
+              bgSplitterCtrl.hidePane(scope.index - 1);
+            } else {
+              bgSplitterCtrl.showPane();
+            }
+          }
+        });
+      })
     }
   };
 });
